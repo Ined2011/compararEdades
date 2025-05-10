643993b8d13994ab0ff7f5c51ce8431ce0105ac0
ined2011 <mayerlindained@gmail.com>
2025-05-10 10:55:08 -0500

    Se realiza creacion de repositorio GiHub para registras 2 commits en el ejercicio ya desarrolado d elas edades, realizando camelCase

---
+++ src/compararEdades.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class compararEdades {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa tu edad: ");
+        int edad1 = sc.nextInt();
+        System.out.println("Ingresa la edad de tu compañero: ");
+        int edad2 = sc.nextInt();
+        if(edad1 > edad2){
+            System.out.println("La primera persona es mayor ");
+        }
+        sc.close();
+    }
+}
